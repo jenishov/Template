af89ddf779e8c8df5fbef3cefc61d50e883a5d56
jenishov <adilet.jenishov@mail.ru>
2022-05-07 20:30:05 +0600

    first add

---
+++ crs/js/main.js
@@ -0,0 +1,5 @@
+document.querySelector('.burger').onclick = function () {
+document.querySelector('.burger').classList.toggle('burger_active')
+    document.querySelector('.header__list').classList.toggle('header__list_active')
+
+}
